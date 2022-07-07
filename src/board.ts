725ce7bd8176414e0cd77b57a4bce4a16e02df3d
Emre Guneyler <eguneys@gmail.com>
2022-07-08 00:37:07 +0300

    add tests

--- src/board.ts
+++ src/board.ts
@@ -3,9 +3,22 @@ import { Pieses, Piese } from './types'
 
 export class Board {
 
+  static from_pieses = (_pieses: Pieses) => {
+    return new Board(new Map(_pieses.map(_ => {
+      let [piece, pos] = _.split('@')
 
-  constructor(readonly pieses: Pieses) {}
+      return [pos, piece]
+    })))
+  }
 
+  get pieses() {
+    return [...this._pieses].map(_ => _.reverse().join('@'))
+  }
 
+  in(_: Piese) {
+    let [piece, pos] = _.split('@')
+    this._pieses.set(pos, piece)
+  }
 
+  constructor(readonly _pieses: Map<Pos, Piece>) {}
 }
--- tests/board.test.ts
+++ tests/board.test.ts
@@ -3,8 +3,13 @@ import { Board } from '../src'
 
 it('should drop pieces', async () => {
 
-  console.log(new Board())
+  let board = Board.from_pieses([])
 
 
+  board.in('wr@a1')
+  board.in('bq@a2')
+
+  expect(board.pieses)
+  .toStrictEqual(['wr@a1', 'bq@a2'])
 })
 
