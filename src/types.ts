d3a29490e949c6698bf20e25769d220dd362c09c
Emre Guneyler <eguneys@hotmail.com>
2022-07-09 21:42:02 +0000

    rays

--- src/types.ts
+++ src/types.ts
@@ -30,6 +30,27 @@ export const left = { 'h': 'g', 'g': 'f', 'f': 'e', 'e': 'd', 'd': 'c', 'c': 'b'
 export const up = { '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8'}
 export const down = { '8': '7', '7': '6', '6': '5', '5': '4', '4': '3', '3': '2', '2': '1'}
 
+export function make_right2(files: Array<A>, right: any) {
+  let res = {}
+
+  files.forEach(file => {
+    let i = right[file]
+    if (i) {
+      i= right[i]
+
+      if (i) {
+        res[file] = i
+      }
+    }
+  })
+  return res
+}
+
+export const right2 = make_right2(files, right)
+export const left2 = make_right2(files, left)
+export const up2 = make_right2(ranks, up)
+export const down2 = make_right2(ranks, down)
+
 export function make_righter<A>(files: Array<A>, right: any) {
   let res = {}
 
@@ -108,15 +129,108 @@ export const backward = make_forward(downer)
 function zip_pos<A, B>(a: Array<A>, b: Array<B>) {
   return a.map((_a, i) => _a + b[i])
 }
-export function make_fwd_que() {
+export function make_fwd_que(upper: any, lefter: any) {
   let res = {}
 
   poss.forEach(pos => {
-    let _upper = upper[pos]
-    let _lefter = lefter[pos]
+    let [file, rank] = pos.split('')
+    let _res = {}
 
-    res[pos] = zip_pos(_upper, _lefter)
+    let _upper = upper[rank]
+    let _lefter = lefter[file]
+
+    Object.keys(_upper).map(_up => {
+      let _left = Object.keys(_lefter).find(_left =>
+        _lefter[_left].length === 
+        _upper[_up].length)
+      if (_left) {
+
+        let n = zip_pos(_lefter[_left], _upper[_up])
+
+        _res[_left + _up] = n
+      }
+    })
+    res[pos] = _res
   })
 
   return res
 }
+
+export const fwd_que = make_fwd_que(upper, lefter)
+export const fwd_kng = make_fwd_que(upper, righter)
+export const bck_que = make_fwd_que(downer, lefter)
+export const bck_kng = make_fwd_que(downer, righter)
+
+export function make_fwd2_que(up2: any, left: any) {
+  let res = {}
+
+  poss.forEach(pos => {
+    let [file, rank] = pos.split('')
+    poss.forEach(_pos => {
+    let [_file, _rank] = _pos.split('')
+
+      if (left[file] === _file && up2[rank] === _rank) {
+        res[pos] = _pos
+      }
+    })
+  })
+
+  return res
+}
+
+export const fwd2_que = make_fwd2_que(up2, left)
+export const fwd2_kng = make_fwd2_que(up2, right)
+export const fwd_que2 = make_fwd2_que(up, left2)
+export const fwd_kng2 = make_fwd2_que(up, right2) 
+
+export const bck2_que = make_fwd2_que(down2, left)
+export const bck2_kng = make_fwd2_que(down2, right)
+export const bck_que2 = make_fwd2_que(down, left2)
+export const bck_kng2 = make_fwd2_que(down, right2) 
+
+
+export function make_fwdn(forward: any, n: number) {
+  let res = {}
+
+  poss.forEach(pos => {
+    let _pos = Object.keys(forward[pos]).find(_ => forward[pos][_].length === n)
+
+    if (_pos) {
+
+      res[pos] = { [_pos]: forward[pos][_pos] }
+    }
+  })
+
+  return res 
+}
+
+export const make_fwd2 = (forward: any) => make_fwdn(forward, 1)
+export const make_fwd1 = (forward: any) => make_fwdn(forward, 0)
+
+export const fwd2 = make_fwd2(forward)
+export const bck2 = make_fwd2(backward)
+
+
+
+export function xy_or(ors: Array<any>) {
+  let res = {}
+
+  poss.forEach(pos => {
+    let _res = []
+    poss.forEach(_pos => {
+      if (ors.map(_ => _[pos] === _pos).find(Boolean)) {
+        _res.push(_pos)
+      }
+    })
+    if (_res.length > 0) {
+      res[pos] = _res
+    }
+  })
+  return res
+}
+
+export const knight = xy_or([fwd2_que, fwd2_kng, bck2_que, bck2_kng, fwd_que2, fwd_kng2, bck_que2, bck_kng2])
+
+
+
+
--- tests/rays.test.ts
+++ tests/rays.test.ts
@@ -1,7 +1,13 @@
-import { left, lefter, right, righter, king_side, queen_side, forward, backward } from '../src'
+import { left, lefter, right, righter, king_side, queen_side, forward, backward, fwd_que, fwd2_que, fwd2_kng, fwd_que2, fwd_kng2, fwd2 } from '../src'
+
+import { knight } from '../src'
+
+it('should knight', () => {
+  expect(knight['d4'].slice(0).sort()).toStrictEqual(['c6', 'e2', 'e6', 'c2', 'f3', 'f5', 'b3', 'b5'].sort())
+})
 
 
-it('should find right', async () => {
+it('should find right', () => {
 
   expect(righter['a']['h']).toStrictEqual(['b', 'c', 'd', 'e', 'f', 'g'])
   expect(lefter['h']['a']).toStrictEqual(['b', 'c', 'd', 'e', 'f', 'g'].reverse())
@@ -11,4 +17,12 @@ it('should find right', async () => {
 
 
   expect(forward['g4']['g8']).toStrictEqual(['g5', 'g6', 'g7'])
+
+  expect(fwd_que['g4']['c8']).toStrictEqual(['f5', 'e6', 'd7'])
+
+  expect(fwd2_que['c4']).toStrictEqual('b6')
+  expect(fwd2_kng['c4']).toStrictEqual('d6')
+  expect(fwd_que2['c4']).toStrictEqual('a5')
+  expect(fwd_kng2['c4']).toStrictEqual('e5')
 })
+
